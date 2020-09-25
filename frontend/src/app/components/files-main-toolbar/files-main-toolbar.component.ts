5ba25ae633563b39d0bd6c4726598ff58f70d4c6
Clément FORGEARD <clement_forgeard@outlook.fr>
2020-09-25 11:43:28 +0200

    hide menu button if treeview sidenav is locked

--- frontend/src/app/components/files-main-toolbar/files-main-toolbar.component.ts
+++ frontend/src/app/components/files-main-toolbar/files-main-toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-files-main-toolbar',
@@ -7,6 +7,7 @@ import { Component, Output, EventEmitter } from '@angular/core';
 })
 export class FilesMainToolbarComponent {
 
+  @Input() hideMenuButton: boolean;
   @Output() menuButtonClicked = new EventEmitter<void>();
   
   constructor() { }
