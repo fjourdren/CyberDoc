775c9d4958e2d0500ce8035a50cd1100ddd61f2d
Clément FORGEARD <clement_forgeard@outlook.fr>
2020-09-28 14:14:51 +0200

    error handler redirect only if prod environ

--- frontend/src/app/global-error-handler.ts
+++ frontend/src/app/global-error-handler.ts
@@ -1,10 +1,13 @@
 import { ErrorHandler, Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
 
     handleError(error) {
         console.error(error);
-        location.pathname = "/error";
+        if (environment.production) {
+            history.pushState({}, null, `/error`);
+        }
     }
 }
