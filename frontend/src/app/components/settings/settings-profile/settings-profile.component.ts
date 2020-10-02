32c088f9a06047fec6d582d154786b80b63e483f
Clément FORGEARD <clement_forgeard@outlook.fr>
2020-10-02 21:23:12 +0200

    [SettingsProfileComponent] add email validator + snackbar + fix updateProfil()

--- frontend/src/app/components/settings/settings-profile/settings-profile.component.ts
+++ frontend/src/app/components/settings/settings-profile/settings-profile.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { UserServiceProvider } from 'src/app/services/users/user-service-provider';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-settings-profile',
   templateUrl: './settings-profile.component.html',
@@ -14,20 +16,20 @@ export class SettingsProfileComponent {
     oldEmail: new FormControl('')
   });
 
-  constructor(private userServiceProvider: UserServiceProvider, private fb: FormBuilder) { }
+  constructor(private userServiceProvider: UserServiceProvider, private fb: FormBuilder, private snackBar: MatSnackBar) { }
 
   ngOnInit() {
     this.profileForm = this.fb.group({
       firstName: [this.userServiceProvider.default().getActiveUser().firstname, Validators.required],
       lastName: [this.userServiceProvider.default().getActiveUser().lastname, Validators.required],
-      newEmail: [this.userServiceProvider.default().getActiveUser().email, Validators.required],
-      oldEmail: [this.userServiceProvider.default().getActiveUser().email, Validators.required]
+      newEmail: [this.userServiceProvider.default().getActiveUser().email, [Validators.required, Validators.email]],
+      oldEmail: [this.userServiceProvider.default().getActiveUser().email, [Validators.required, Validators.email]]
     });
   }
 
   onSubmit() {
     console.warn(this.profileForm.value);
-    this.updateProfile(); 
+    this.updateProfile();
   }
 
   updateProfile() {
@@ -35,8 +37,8 @@ export class SettingsProfileComponent {
       this.profileForm.get('firstName').value,
       this.profileForm.get('lastName').value,
       this.profileForm.get('newEmail').value,
-      this.profileForm.get('oldEmail').value
-    );
+      this.userServiceProvider.default().getActiveUser().email
+    ).toPromise().then(() => this.snackBar.open('Profile updated', null, { duration: 1500 })).catch(err => console.error(err));
   }
 
   deleteAccount() {
