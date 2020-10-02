0c3eb7af427c0e345c609c3812d4cb943c28a7ca
Clément FORGEARD <clement_forgeard@outlook.fr>
2020-10-02 20:46:42 +0200

    better mock-user-service

--- frontend/src/app/services/users/mock-user-service.ts
+++ frontend/src/app/services/users/mock-user-service.ts
@@ -2,11 +2,12 @@ import { UserService } from './user-service';
 
 import { Observable, of } from 'rxjs';
 import { User } from 'src/app/models/users-api-models';
-import { delay } from 'rxjs/operators';
+import { delay, map } from 'rxjs/operators';
 
 const LOCALSTORAGE_KEY = "auth-token";
 const JWT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InJvbGUiOiJjb2xsYWJvcmF0ZXIiLCJ1cGRhdGVkX2F0IjoiMjAyMC0wOS0yMlQxMTozMToyMC43MTRaIiwiY3JlYXRlZF9hdCI6IjIwMjAtMDktMjJUMTE6MzA6NTQuNTU2WiIsIl9pZCI6IjY1YWY4OGUwLTRkNmYtODBkYS0xY2FiLTZlZjVkYjJjNzE5ZSIsImZpcnN0bmFtZSI6IkZsYXZpZW4iLCJsYXN0bmFtZSI6IkpPVVJEUkVOIiwiZW1haWwiOiJmbGF2aWVuLmpvdXJkcmVuQGdtYWlsLmNvbSJ9LCJpYXQiOjE2MDA3NzQyODMsImV4cCI6MTYwMDg2MDY4M30.kHhr6DWSg1ZLkmBFH5FTLbDtTpoX9HGKv0ewmUkQFK8";
 const DELAY = 500;
+let nextID = 0;
 const USER: User = {
     "role": "owner",
     "updated_at": "2020-09-22T11:31:20.714Z",
@@ -19,16 +20,12 @@ const USER: User = {
 }
 
 export class MockUserService implements UserService {
-    private _activeUser: User;
     private _users = new Map<string, User>(); //email -> user
+    private _passwords = new Map<string, string>(); //email -> password
 
     constructor() {
         localStorage.setItem(LOCALSTORAGE_KEY, JWT_TOKEN);
-        this._users.set(USER.email, USER);
-        if (localStorage.getItem("__currentUser") == undefined) {
-            localStorage.setItem("__currentUser", JSON.stringify(USER));
-        }
-        this._activeUser = USER;
+        this._load();
     }
 
     getJwtToken(): string {
@@ -36,6 +33,116 @@ export class MockUserService implements UserService {
     }
 
     getActiveUser(): User {
+        return this._getUser();
+    }
+
+    register(user: User, password: string): Observable<User> {
+        return of(null).pipe(delay(DELAY)).pipe(map(() => {
+            if (this.getActiveUser()) {
+                throw new Error("403 already logged in");
+            }
+
+            if (this._users.has(user.email)) {
+                throw new Error("409 already existing user");
+            }
+
+            user._id = `${nextID++}`;
+            user.rootDirectoryID = "other.root";
+            user.created_at = new Date().toISOString();
+            user.updated_at = new Date().toISOString();
+
+            this._users.set(user.email, user);
+            this._passwords.set(user.email, password);
+            this._setUser(null);
+            this._save();
+            return user;
+        }));
+    }
+
+    updateProfile(firstName: string, lastName: string, newEmail: string, oldEmail: string) {
+        return of(null).pipe(delay(DELAY)).pipe(map(() => {
+            if (!this.getActiveUser()) {
+                throw new Error("403 not logged in");
+            }
+
+            const user = this._users.get(oldEmail);
+            const pass = this._passwords.get(oldEmail);
+            user.firstname = firstName;
+            user.lastname = lastName;
+            user.email = newEmail;
+            this._users.delete(oldEmail);
+            this._passwords.delete(oldEmail);
+            this._users.set(newEmail, user);
+            this._passwords.set(newEmail, pass);
+            this._save();
+            this._setUser(user);
+        }));
+    }
+
+    updatePassword(oldPassword: string, newPassword: string, email: string) {
+        return of(null).pipe(delay(DELAY)).pipe(map(() => {
+            if (!this.getActiveUser()) {
+                throw new Error("403 not logged in");
+            }
+
+            const pass = this._passwords.get(email);
+            if (pass !== oldPassword) {
+                throw new Error("403 WRONG PASSWORD");
+            }
+
+            this._passwords.set(email, newPassword);
+            this._save();
+        }));
+    }
+
+    login(email: string, password: string): Observable<User> {
+        return of(null).pipe(delay(DELAY)).pipe(map(() => {
+            if (this.getActiveUser()) {
+                throw new Error("403 already logged");
+            }
+
+            for (const user of this._users.values()) {
+                if (user.email === email) {
+                    const pass = this._passwords.get(email);
+                    if (pass !== password) {
+                        throw new Error("404 wrong email or password");
+                    }
+                    this._setUser(user);
+                    this._save();
+                    return user;
+                }
+            }
+
+            throw new Error("404 wrong email or password");
+        }));
+    }
+
+    logout(): Observable<void> {
+        return of(null).pipe(delay(DELAY)).pipe(map(() => {
+            if (!this.getActiveUser()) {
+                throw new Error("403 not logged in");
+            }
+
+            this._setUser(null);
+            this._save();
+        }));
+    }
+
+    deleteAccount(): Observable<void> {
+        return of(null).pipe(delay(DELAY)).pipe(map(() => {
+            if (!this.getActiveUser()) {
+                throw new Error("403 not logged in");
+            }
+
+            const user = this.getActiveUser();
+            this._users.delete(user.email);
+            this._passwords.delete(user.email);
+            this._setUser(null);
+            this._save();
+        }));
+    }
+
+    private _getUser(): User {
         const val = JSON.parse(localStorage.getItem("__currentUser"));
         if (val === "null") {
             return null;
@@ -44,65 +151,29 @@ export class MockUserService implements UserService {
         }
     }
 
-    register(user: User): Observable<User> {
-        if (this._users.has(user.email)) {
-            throw new Error("409 already existing user");
+    private _setUser(user: User) {
+        if (user) {
+            localStorage.setItem("__currentUser", JSON.stringify(user));
+        } else {
+            localStorage.setItem("__currentUser", JSON.stringify("null"));
         }
-
-        this._users.set(user.email, user);
-        return of(user).pipe(delay(DELAY));
     }
 
-    updateProfile(firstName: string, lastName: string, newEmail: string, oldEmail: string) {
-        if (!this._activeUser) {
-            throw new Error("403 not logged in");
-        }
-
-        const user = this._users.get(oldEmail);
-        if (user) { 
-            user.firstname = firstName;
-            user.lastname = lastName;
-            user.email = newEmail;
-            this._users.delete(oldEmail);
-            this._users.set(newEmail, user);
-            localStorage.setItem("__currentUser", JSON.stringify(user))
-            return of(null).pipe(delay(DELAY));
-        } else throw new Error("Account doesn't exist");
+    private _save(){
+        localStorage.setItem("__users", JSON.stringify(Array.from(this._users.entries())))
+        localStorage.setItem("__passwords", JSON.stringify(Array.from(this._passwords.entries())))
     }
 
-    updatePassword(oldPassword: string, newPassword: string, email: string) {
-        if (!this._activeUser) {
-            throw new Error("403 not logged in");
+    private _load(){
+        this._users = new Map(JSON.parse(localStorage.getItem("__users")));
+        this._passwords = new Map(JSON.parse(localStorage.getItem("__passwords")));
+
+        if (!this._users || this._users.size === 0) {
+            this._users = new Map();
+            this._passwords = new Map();
+            this._users.set(USER.email, USER);
+            this._passwords.set(USER.email, "Chb44xw@@Q");
+            this._save();    
         }
-
-        const user = this._users.get(email);
-        if (user != undefined) { 
-            // TODO : Changer le mdp
-            return of(null).pipe(delay(DELAY));
-        } else throw new Error("Account doesn't exist");
-    }
-
-    login(email: string, password: string): Observable<User> {
-        for (const user of this._users.values()) {
-            if (user.email === email) {
-                return of(user).pipe(delay(DELAY));
-            }
-        }
-        throw new Error("404 not found");
-    }
-
-    logout(): Observable<void> {
-        localStorage.setItem("__currentUser", JSON.stringify("null"));
-        return of(null).pipe(delay(DELAY));
-    }
-
-    deleteAccount(): Observable<void> {
-        if (!this._activeUser) {
-            throw new Error("403 not logged in");
-        }
-
-        this._users.delete(this._activeUser.email);
-        this._activeUser = null;
-        return of(null).pipe(delay(DELAY)).pipe();
     }
 }
