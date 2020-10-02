36930ce45ae2d712a5d93668e4e1075b2acbf1c7
Clément FORGEARD <clement_forgeard@outlook.fr>
2020-10-02 21:00:33 +0200

    add Logged{In,Out}Guard to app-routing.module.ts + redirection for /settings

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthGuard } from './guards/auth/auth.guard';
 
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
@@ -9,18 +8,24 @@ import { LogoutPageComponent } from './pages/logout-page/logout-page.component';
 import { FilesPageComponent } from './pages/files-page/files-page.component';
 import { SettingsProfilePageComponent } from './pages/settings-profile-page/settings-profile-page.component';
 import { SettingsSecurityPageComponent } from './pages/settings-security-page/settings-security-page.component';
+import { LoggedInGuard } from './guards/logged-in/logged-in.guard';
+import { LoggedOutGuard } from './guards/logged-out/logged-out.guard';
 
 
 const routes: Routes = [
+
   { path: '', redirectTo: 'files', pathMatch: 'full' },
-  { path: 'files/:dirID', component: FilesPageComponent, canActivate: [AuthGuard] },
-  { path: 'files', component: FilesPageComponent, canActivate: [AuthGuard] },
-  { path: 'logout', component: LogoutPageComponent, canActivate: [AuthGuard] },
-  { path: 'login', component: LoginPageComponent },
-  { path: 'register', component: RegisterPageComponent },
-  { path: 'settings', component: SettingsProfilePageComponent, canActivate: [AuthGuard] },
-  { path: 'settings/profile', component: SettingsProfilePageComponent, canActivate: [AuthGuard] },
-  { path: 'settings/security', component: SettingsSecurityPageComponent, canActivate: [AuthGuard] },
+  { path: 'files/:dirID', component: FilesPageComponent, canActivate: [LoggedInGuard] },
+  { path: 'files', component: FilesPageComponent, canActivate: [LoggedInGuard] },
+
+  { path: 'logout', component: LogoutPageComponent, canActivate: [LoggedInGuard] },
+  { path: 'login', component: LoginPageComponent, canActivate: [LoggedOutGuard] },
+  { path: 'register', component: RegisterPageComponent, canActivate: [LoggedOutGuard] },
+
+  { path: 'settings', redirectTo: 'settings/profile', pathMatch: 'full' },
+  { path: 'settings/profile', component: SettingsProfilePageComponent, canActivate: [LoggedInGuard] },
+  { path: 'settings/security', component: SettingsSecurityPageComponent, canActivate: [LoggedInGuard] },
+
   { path: '**', component: NotFoundPageComponent },
 ];
 
