ef149ab26e56ddecb57c98788810b7567573e1cc
Clément FORGEARD <clement_forgeard@outlook.fr>
2020-10-02 21:16:13 +0200

    [SettingsSecurityComponent] add success snackbar and fix updatePassword

--- frontend/src/app/components/settings/settings-security/settings-security.component.ts
+++ frontend/src/app/components/settings/settings-security/settings-security.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Form, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { UserServiceProvider } from 'src/app/services/users/user-service-provider';
 
 import { MustMatch } from './_helpers/must-match.validator';
@@ -15,7 +16,7 @@ export class SettingsSecurityComponent implements OnInit {
   isTextFieldType2: boolean;
   isTextFieldType3: boolean;
 
-  constructor(private userServiceProvider: UserServiceProvider, private fb: FormBuilder) { }
+  constructor(private userServiceProvider: UserServiceProvider, private fb: FormBuilder, private snackBar: MatSnackBar) { }
 
   passwordStrength = '(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}';
 
@@ -34,33 +35,33 @@ export class SettingsSecurityComponent implements OnInit {
 
   onSubmit() {
     console.warn(this.securityForm.value);
-    if(this.securityForm.get('oldPassword').value !== null && this.securityForm.get('newPassword').value !== null 
+    if (this.securityForm.get('oldPassword').value !== null && this.securityForm.get('newPassword').value !== null
       && this.securityForm.get('newPasswordConfirmation').value !== null) {
-      this.updatePassword(); 
+      this.updatePassword();
     }
   }
 
   updatePassword() {
-    if(this.securityForm.get('newPassword').value != this.securityForm.get('newPasswordConfirmation').value) {
+    if (this.securityForm.get('newPassword').value != this.securityForm.get('newPasswordConfirmation').value) {
       console.log('Passwords must match')
     } else {
       this.userServiceProvider.default().updatePassword(
         this.securityForm.get('oldPassword').value,
         this.securityForm.get('newPassword').value,
-        this.securityForm.get('email').value
-      );
+        this.userServiceProvider.default().getActiveUser().email
+      ).toPromise().then(() => this.snackBar.open('Password updated', null, { duration: 1500 })).catch(err => this.snackBar.open(err, null, { duration: 1500 }));
     }
   }
 
-  toggleOldPasswordFieldType(evt: Event){
+  toggleOldPasswordFieldType(evt: Event) {
     evt.preventDefault();
     this.isTextFieldType = !this.isTextFieldType;
   }
-  toggleNewPasswordFieldType(evt: Event){
+  toggleNewPasswordFieldType(evt: Event) {
     evt.preventDefault();
     this.isTextFieldType2 = !this.isTextFieldType2;
   }
-  toggleNewPasswordConfirmFieldType(evt: Event){
+  toggleNewPasswordConfirmFieldType(evt: Event) {
     evt.preventDefault();
     this.isTextFieldType3 = !this.isTextFieldType3;
   }
