48999158b34e60cc4acf3a0d86514ce858ad53c0
Clément FORGEARD <clement_forgeard@outlook.fr>
2020-10-10 09:04:57 +0200

    some css fixes for login-screen

--- frontend/src/app/pages/login-page/login-page.component.ts
+++ frontend/src/app/pages/login-page/login-page.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.scss']
 })
 
 export class LoginPageComponent {
