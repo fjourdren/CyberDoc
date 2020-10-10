eb37a835ffcb77872a12373d690b3f23f5f0c2cc
Clément FORGEARD <clement_forgeard@outlook.fr>
2020-10-10 08:39:45 +0200

    refactor login-page

--- frontend/src/app/pages/login-page/login-page.component.ts
+++ frontend/src/app/pages/login-page/login-page.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, Validators } from '@angular/forms';
-import {UserServiceProvider} from '../../services/users/user-service-provider'
-import { first } from 'rxjs/operators';
-import { User } from 'src/app/models/users-api-models';
-import { error } from 'console';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { UserServiceProvider } from '../../services/users/user-service-provider'
 import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -12,70 +10,45 @@ import { HttpErrorResponse } from '@angular/common/http';
   styleUrls: ['./login-page.component.css']
 })
 
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent {
   loginForm = this.fb.group({
-    email: [null, Validators.required],
+    email: [null, [Validators.required, Validators.email]],
     password: [null, Validators.required],
-    state: ['owner', Validators.required],
   });
 
-  hide = true;
-  hasUnitNumber = false;
+  hidePassword = true;
+  loading = false;
   errorValidator = false;
   errorServer = false;
 
- // email = new FormControl('', [Validators.required, Validators.email]);
-
-  constructor(private fb: FormBuilder, private user: UserServiceProvider) { }
+  constructor(private fb: FormBuilder,
+    private user: UserServiceProvider,
+    private router: Router) { }
 
   onSubmit() {
-    if(this.loginForm.invalid){
-      //alert('Error !');
+    if (this.loginForm.invalid) {
       return;
     }
-    
+
+    this.loading = true;
+    this.errorServer = false;
+    this.errorValidator = false;
+    this.loginForm.get("email").disable();
+    this.loginForm.get("password").disable();
+
     this.user.default().login(this.loginForm.controls.email.value, this.loginForm.controls.password.value).toPromise().then(value => {
-      console.log(value);
-      localStorage.setItem("__currentUser", JSON.stringify(value));
-      location.pathname = "/files";
-    },value =>{
-        //In prod
-        if(value instanceof HttpErrorResponse)
-        { 
-          this.onError(value);
-        }
-        //In mock :
-        if(value instanceof Error)
-        { 
-          this.onErrorMock(value);
-        }
-        });
-  }
-
-  onError(value:HttpErrorResponse){
-    console.log(value.status);
-    if(value.status==404){
-      this.errorValidator=true;
-    }
-    else{
-      this.errorServer=true;
-    }
-  }
-
-  onErrorMock(value:Error){
-    console.log("In mock");
-    if(value.message.includes("404")){
-      this.errorValidator=true;
-    }
-    else{
-      this.errorServer=true;
-    }
-  }
-
-  ngOnInit() {
-    this.loginForm = this.fb.group({
-      email: [null, [Validators.required, Validators.email]],
-      password: [null, Validators.required],
+      this.loading = false;
+      this.router.navigate(["/files"]);
+    }, error => {
+      this.loading = false;
+      this.loginForm.get("email").enable();
+      this.loginForm.get("password").enable();
+  
+      if (error instanceof HttpErrorResponse && error.status == 404) {
+        this.errorValidator = true;
+      } else {
+        this.errorServer = true;
+      }
     });
   }
 }
