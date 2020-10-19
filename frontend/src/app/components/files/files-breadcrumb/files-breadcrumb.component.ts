d7436a08a62df30f5c4a4adc046bbfb2a66e536c
Clément FORGEARD <clement_forgeard@outlook.fr>
2020-10-19 15:35:24 +0200

    fix breadcrumb bug #2

--- frontend/src/app/components/files/files-breadcrumb/files-breadcrumb.component.ts
+++ frontend/src/app/components/files/files-breadcrumb/files-breadcrumb.component.ts
@@ -16,8 +16,8 @@ export class FilesBreadcrumbComponent {
 
   constructor(private fsProvider: FileSystemProvider) { }
 
-  loadDataForContextMenu(directory: CloudDirectory) {
-    this.contextMenuContent$ = this.fsProvider.default().get(directory._id).pipe(map((val) => {
+  loadDataForContextMenu(directoryID: string) {
+    this.contextMenuContent$ = this.fsProvider.default().get(directoryID).pipe(map((val) => {
       if (val.isDirectory) {
         return val.directoryContent.filter(v => v.isDirectory) as CloudDirectory[];
       }
