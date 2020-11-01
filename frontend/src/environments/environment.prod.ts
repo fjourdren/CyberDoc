de5a579608cacc7a4725700e7a65cd46728edcfc
Clément FORGEARD <clement_forgeard@outlook.fr>
2020-11-01 15:47:20 +0100

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   useSentry: false,
   defaultFSProviderName: "real",
   defaultUserServiceName: "real",
-  apiBaseURL: "http://api.cyberdoc.fulgen.fr/v1",
+  apiBaseURL: "https://api.cyberdoc.fulgen.fr/v1",
   authCookieDomain: "cyberdoc.fulgen.fr",
   authCookieName: "access_token"
 };
