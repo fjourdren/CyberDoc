d000f38cd5ec4eea0ac05170afe15d8c7c14d998
Maeg <37983763+Danmaek@users.noreply.github.com>
2020-11-17 16:19:18 +0100

    Update exec.sh

--- devops/docker_backend/exec.sh
+++ devops/docker_backend/exec.sh
@@ -4,16 +4,16 @@ echo "INSTALLING"
 npm install
 echo "BUILDING"
 npm run build
-echo "DELETED USELESS FILES"
-rm -rf .git
-rm -rf src/
-rm .env.example
-rm .eslintignore
-rm .eslintrc
-rm .gitignore
-rm dockerfile
-rm jest.config.js
-rm nodemon.json
-rm tsconfig.json
+#echo "DELETED USELESS FILES"
+#rm -rf .git
+#rm -rf src/
+#rm .env.example
+#rm .eslintignore
+#rm .eslintrc
+#rm .gitignore
+#rm dockerfile
+#rm jest.config.js
+#rm nodemon.json
+#rm tsconfig.json
 echo "STARTING BACKEND"
 npm run start
