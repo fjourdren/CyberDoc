4e7a020568bd241526bcb9cb3a9d5bd97ce3afd9
Maeg <37983763+Danmaek@users.noreply.github.com>
2020-12-17 13:52:41 +0100

    update exec.sh
    for migration backend to nest

--- devops/docker_backend/exec.sh
+++ devops/docker_backend/exec.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
 # chmod 777 exec.sh
+#echo "INSTALLING"
+#npm install
+#echo "BUILDING"
+#npm run build
+#echo "DELETED USELESS FILES"
+#rm -rf .git
+#rm -rf src/
+#rm .env.example
+#rm .eslintignore
+#rm .eslintrc
+#rm .gitignore
+#rm dockerfile
+#rm jest.config.js
+#rm nodemon.json
+#rm tsconfig.json
+#echo "STARTING BACKEND"
+#npm run start
+
+
+
 echo "INSTALLING"
 npm install
 echo "BUILDING"
 npm run build
 echo "DELETED USELESS FILES"
-rm -rf .git
 rm -rf src/
-rm .env.example
-rm .eslintignore
-rm .eslintrc
+rm -rf test/
+rm .eslintrc.js
 rm .gitignore
-rm dockerfile
-rm jest.config.js
-rm nodemon.json
+rm .prettierrc
+rm README.md
+rm nest-cli.json
+rm package-lock.json
+rm package.json
+rm tsconfig.build.json
 rm tsconfig.json
 echo "STARTING BACKEND"
 npm run start
