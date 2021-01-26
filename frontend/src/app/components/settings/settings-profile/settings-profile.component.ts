7e2b897818072356170388438814cee9397263c9
Clément FORGEARD <clement_forgeard@outlook.fr>
2021-01-26 20:37:09 +0100

    [frontend] fix settings-billing-card margin

--- frontend/src/app/components/settings/settings-profile/settings-profile.component.ts
+++ frontend/src/app/components/settings/settings-profile/settings-profile.component.ts
@@ -18,7 +18,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-settings-profile',
   templateUrl: './settings-profile.component.html',
-  styleUrls: ['./settings-profile.component.scss'],
+  styleUrls: ['./settings-profile.component.css'],
 })
 export class SettingsProfileComponent {
   VALID_THEMES = [
